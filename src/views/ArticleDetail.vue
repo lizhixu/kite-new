bb89db064785a2fc76f6dbc617fd2df2db9d897b
lizhixu <lzx@edu.email.cn>
2024-02-06 12:04:26 +0800

    feat: md渲染超链接新建页面打开链接

--- src/views/ArticleDetail.vue
+++ src/views/ArticleDetail.vue
@@ -130,6 +130,22 @@ const md = new MarkdownIt({
 })
 md.use(MarkdownItGithubHeadings);
 md.use(MarkdownItCopy, {iconClass: 'iconfont icon-Copy', buttonClass: 'el-button el-button--primary is-link'});
+let defaultRender = md.renderer.rules.link_open || function(tokens, idx, options, env, self) {
+  return self.renderToken(tokens, idx, options);
+};
+md.renderer.rules.link_open = function (tokens, idx, options, env, self) {
+  // If you are sure other plugins can't add `target` - drop check below
+  var aIndex = tokens[idx].attrIndex('target');
+
+  if (aIndex < 0) {
+    tokens[idx].attrPush(['target', '_blank']); // add new attribute
+  } else {
+    tokens[idx].attrs[aIndex][1] = '_blank';    // replace value of existing attr
+  }
+
+  // pass token to default renderer.
+  return defaultRender(tokens, idx, options, env, self);
+};
 
 const route = useRoute();
 const loading = ref(true);
